2aa7c19448626892ab94f5632b2acde337add10a
anon <sarah.anne.friend@gmail.com>
2020-04-20 17:08:07 -0400

    Compiles

--- src/culturestake.ts
+++ src/culturestake.ts
@@ -11,7 +11,7 @@
   DeactivateQuestion as DeactivateQuestionEvent,
   DeactivateFestival as DeactivateFestivalEvent,
   DeactivateVotingBooth as DeactivateVotingBoothEvent,
-} from './types/Hub/Hub'
+} from './types/Culturestake/Culturestake'
 
 import {
   Question,
@@ -23,14 +23,53 @@ import {
   Question as QuestionContract,
 } from './types/templates'
 
-export function handleInitQuestion(event: InitQuestionEvent): void { }
+export function handleInitQuestion(event: InitQuestionEvent): void {
+  QuestionContract.create(event.params.questionAddress)
 
-export function handleInitFestival(event: InitFestivalEvent): void { }
+  let question = new Question(event.params.questionAddress.toHexString())
+  question.questionType = new BigInt(event.params.questionType)
+  question.festival = event.params.festival.toString()
+  question.active = true
+  question.save()
 
-export function handleInitVotingBooth(event: InitVotingBoothEvent): void { }
+  let fest = Festival.load(event.params.festival.toString())
+  fest.questions.push(event.params.questionAddress.toHexString())
+  fest.save()
+}
 
-export function handleDeactivateQuestion(event: DeactivateQuestionEvent): void { }
+export function handleInitFestival(event: InitFestivalEvent): void {
+  let festival = new Festival(event.params.festival.toString())
+  festival.active = true
+  festival.startTime = event.params.startTime
+  festival.duration = event.params.duration
+  festival.save()
+}
 
-export function handleDeactivateFestival(event: DeactivateFestivalEvent): void { }
+export function handleInitVotingBooth(event: InitVotingBoothEvent): void {
+  let booth = new VotingBooth(event.params.boothAddress.toHexString())
+  booth.active = true
+  booth.festival = event.params.festival.toString()
+  booth.save()
 
-export function handleDeactivateVotingBooth(event: DeactivateVotingBoothEvent): void { }
+  let fest = Festival.load(event.params.festival.toString())
+  fest.votingBooths.push(event.params.boothAddress.toHexString())
+  fest.save()
+}
+
+export function handleDeactivateQuestion(event: DeactivateQuestionEvent): void { 
+  let question = Question.load(event.params.questionAddress.toHexString())
+  question.active = false
+  question.save()
+}
+
+export function handleDeactivateFestival(event: DeactivateFestivalEvent): void {
+  let fest = Festival.load(event.params.festival.toString())
+  fest.active = false
+  fest.save()
+}
+
+export function handleDeactivateVotingBooth(event: DeactivateVotingBoothEvent): void {
+  let booth = VotingBooth.load(event.params.boothAddress.toHexString())
+  booth.active = false
+  booth.save()
+}
--- src/question.ts
+++ src/question.ts
@@ -16,8 +16,46 @@ import {
   Vote,
 } from './types/schema'
 
-export function handleVote(event: VoteEvent): void { }
+import {
+  Question as QuestionContract,
+} from './types/templates/Question/Question'
 
-export function handleInitAnswer(event: InitAnswerEvent): void { }
 
-export function handleDeactivateAnswer(event: DeactivateAnswerEvent): void { }
+export function createVoteID(booth: Address, nonce: BigInt): string {
+  return booth.toHexString().concat('-').concat(nonce.toString())
+}
+
+export function handleVote(event: VoteEvent): void {
+  let vote = new Vote(createVoteID(event.params.booth, event.params.nonce))
+  vote.booth = event.params.booth.toHexString()
+  vote.answer = event.params.answer.toString()
+  vote.voteTokens = event.params.voteTokens
+  vote.votePower = event.params.votePower
+  vote.count = event.params.votes
+  vote.save()
+
+  let questionContract = QuestionContract.bind(event.address)
+
+  let answer = Answer.load(event.params.answer.toString())
+  let a = questionContract.getAnswer(event.params.answer)
+  answer.voteTokens = a.value2
+  answer.votePower = a.value1
+  answer.votes = a.value3
+  answer.save()
+}
+
+export function handleInitAnswer(event: InitAnswerEvent): void {
+  let answer = new Answer(event.params.answer.toString())
+  answer.voteTokens = new BigInt(0)
+  answer.votePower = new BigInt(0)
+  answer.votes = new BigInt(0)
+  answer.active = true
+  answer.question = event.address.toHexString()
+  answer.save()
+}
+
+export function handleDeactivateAnswer(event: DeactivateAnswerEvent): void {
+  let answer = Answer.load(event.params.answer.toString())
+  answer.active = false
+  answer.save()
+}
